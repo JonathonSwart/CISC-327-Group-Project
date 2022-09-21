ad789b1cd680ee9ac5b45dca867a9a59bb21aa57
Jarvin-N <56123854+Jarvin-N@users.noreply.github.com>
2022-09-21 18:42:15 -0400

    Create Users.py
    Testing some flask and github things.

---
+++ Users.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# setting up SQLAlchemy and data models so we can map data models into database tables
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
