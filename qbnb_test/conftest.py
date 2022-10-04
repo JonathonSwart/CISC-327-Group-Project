4bbffcad664378c913d6fe93826e13a4a0dc2574
Jeevan Narewal <jeevansn1586@gmail.com>
2022-10-04 17:26:57 -0400

    Added a template structure for qbnb

---
+++ qbnb_test/conftest.py
@@ -0,0 +1,23 @@
+import os
+
+'''
+This file defines what to do BEFORE running any test cases:
+'''
+
+
+def pytest_sessionstart():
+    '''
+    Delete database file if existed. So testing can start fresh.
+    '''
+    print('Setting up environment..')
+    db_file = 'db.sqlite'
+    if os.path.exists(db_file):
+        os.remove(db_file)
+
+
+def pytest_sessionfinish():
+    '''
+    Optional function called when testing is done.
+    Do nothing for now
+    '''
+    pass
---
+++ qbnb_test/test_models.py
@@ -0,0 +1,27 @@
+from qbnb.models import register, login
+
+
+def test_r1_7_user_register():
+    '''
+    Testing R1-7: If the email has been used, the operation failed.
+    '''
+
+    assert register('u0', 'test0@test.com', '123456') is True
+    assert register('u0', 'test1@test.com', '123456') is True
+    assert register('u1', 'test0@test.com', '123456') is False
+
+
+def test_r2_1_login():
+    '''
+    Testing R2-1: A user can log in using her/his email address 
+      and the password.
+    (will be tested after the previous test, so we already have u0, 
+      u1 in database)
+    '''
+
+    user = login('test0@test.com', 123456)
+    assert user is not None
+    assert user.username == 'u0'
+
+    user = login('test0@test.com', 1234567)
+    assert user is None
