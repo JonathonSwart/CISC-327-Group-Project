db65e202ea346031056945ff2afb173956998e61
Ash-Taraghi <ash.taraghi@gmail.com>
2022-10-20 12:41:05 -0400

    Added basic templates and starting routes for front-end

--- qbnb/__init__.py
+++ qbnb/__init__.py
@@ -14,6 +14,7 @@ from .models.Booking import Booking
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '69cae04b04756f65eabcd2c5a11c8c24'
 app.app_context().push()
 db.init_app(app)
 db.create_all()
---
+++ qbnb/controllers.py
@@ -0,0 +1,52 @@
+from flask import render_template, request, session, redirect
+from .models.Booking import Booking
+from .models.Listing import Listing
+from .models.Reviews import Reviews
+from .models.User import User
+from .backend_functions import create_listing, update_listing, register, update_profile
+
+from qbnb import app
+
+
+def authenticate(inner_function):
+    """
+    :param inner_function: any python function that accepts a user object
+    Wrap any python function and check the current session to see if 
+    the user has logged in. If login, it will call the inner_function
+    with the logged in user object.
+    To wrap a function, we can put a decoration on that function.
+    Example:
+    @authenticate
+    def home_page(user):
+        pass
+    """
+
+    def wrapped_inner():
+
+        # check did we store the key in the session
+        if 'logged_in' in session:
+            email = session['logged_in']
+            try:
+                user = User.query.filter_by(email=email).one_or_none()
+                if user:
+                    # if the user exists, call the inner_function
+                    # with user as parameter
+                    return inner_function(user)
+            except Exception:
+                pass
+        else:
+            # else, redirect to the login page
+            return redirect('/login')
+
+    # return the wrapped version of the inner_function:
+    return wrapped_inner
+
+
+@app.route('/login', methods=['GET'])
+def login_get():
+    return render_template('login.html')
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('register.html')
